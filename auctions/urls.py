5ec96b805c59579e01eff34f9ab4cadae6ff506e
Liam <helm_of_protection@hotmail.com>
2023-12-24 21:06:16 -0700

    FIxed minor url error

--- auctions/urls.py
+++ auctions/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    
+    path("accounts/login/", views.login_view, name="login"),
     
 ]
